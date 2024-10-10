fe26295fa2d59792b6e32e06cdf73ae439302b09
isabelladutra126 <brunobiletsky@gmail.com>
2024-10-10 17:10:59 -0300

    criando estrutura react para receber a api do dashboard

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <section>
+      <h1>!!! BitWise - Dashboard !!!</h1>
+      <div className='dasboard'>
+        
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default App
